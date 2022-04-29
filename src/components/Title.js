eb9e27e071bf7de28ebabbc498ba51f69362e31d
uenoki <barabaracrm@gmail.com>
2022-04-30 07:20:55 +0900

    selector.jsまで

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+// Title.js
+
+const Titile = () => {
+  return (
+    <div>
+      <h1>Covid Tracker</h1>
+    </div>
+  );
+};
+
+export default Titile;
---
+++ src/pages/TopPagge.js
@@ -0,0 +1,13 @@
+// TopPage.js
+import Title from "../components/Title";
+import Selector from "../components/Selector";
+const TopPage = () => {
+  return (
+    <div>
+      <Title />
+      <Selector />
+    </div>
+  );
+}
+
+export default TopPage;
