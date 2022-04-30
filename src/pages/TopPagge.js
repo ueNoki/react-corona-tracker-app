ea68a917f6dfa9cc7a4f6ffbbe565c40c6c4847e
uenoki <barabaracrm@gmail.com>
2022-04-30 16:55:57 +0900

    app.js変更

--- src/pages/TopPagge.js
+++ src/pages/TopPagge.js
@@ -1,11 +1,13 @@
 // TopPage.js
 import Title from "../components/Title";
 import Selector from "../components/Selector";
+import Results from "../components/Results";
 const TopPage = () => {
   return (
     <div>
       <Title />
       <Selector />
+      <Results />
     </div>
   );
 }
